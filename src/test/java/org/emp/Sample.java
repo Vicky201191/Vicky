1710120316eb879b48db0a84d8dcc1c39d492284
vicky201191 <vickyengg20@gmail.com>
2023-01-27 08:57:13 +0530

    initaite project

---
+++ src/test/java/org/emp/Sample.java
@@ -0,0 +1,19 @@
+package org.emp;
+
+public class Sample {
+	
+	public void method1() {
+		System.out.println("1");
+	}
+	
+	public void method2() {
+		System.out.println("2");
+
+	}
+	
+	public void method3() {
+		System.out.println("3");
+
+	}
+
+}
